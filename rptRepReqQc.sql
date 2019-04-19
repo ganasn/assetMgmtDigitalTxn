268d6b6215c3e8fb5f1156d0d75eb489acc1ad0e
GN-W <ganasn@gmail.com>
2019-04-19 15:11:48 -0700

    fixed tblNote join in reporting requirements SQL to get data based on 1st liens only - 2019.04.19

--- rptRepReqQc.sql
+++ rptRepReqQc.sql
@@ -13,6 +13,7 @@ SET @REP_REQ_STATUS = 'ALL' -- AMRVWDONE OPEN
 IF @REP_REQ_STATUS =  'OPEN'
 BEGIN
 	SELECT 
+		a.ReportingRequirementId AS [MD_REP_REQ_ID],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
 		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
@@ -20,7 +21,7 @@ BEGIN
 		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
 		bb.PropertyName AS [BACKSHOP_PROP_NAME],
 		ii.LoanProgramDesc AS [LOAN_PROGRAM],
-		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], jj. RELATIONSHIP_ID AS [Relationship ID],
 		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
 		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
 		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
@@ -37,7 +38,7 @@ BEGIN
 		INNER JOIN (
 			tblControlMaster aa 
 			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
-			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId
+			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F'
 			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
 			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
 			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
@@ -65,6 +66,7 @@ END
 ELSE IF @REP_REQ_STATUS =  'ALL'
 BEGIN
 	SELECT 
+		a.ReportingRequirementId AS [MD_REP_REQ_ID],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
 		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
@@ -72,7 +74,7 @@ BEGIN
 		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
 		bb.PropertyName AS [BACKSHOP_PROP_NAME],
 		ii.LoanProgramDesc AS [LOAN_PROGRAM],
-		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], jj. RELATIONSHIP_ID AS [Relationship ID],
 		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
 		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
 		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
@@ -89,7 +91,7 @@ BEGIN
 		INNER JOIN (
 			tblControlMaster aa 
 			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
-			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId
+			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F'
 			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
 			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
 			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
@@ -114,6 +116,7 @@ END
 ELSE
 BEGIN
 	SELECT 
+		a.ReportingRequirementId AS [MD_REP_REQ_ID],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
 		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
 		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
@@ -121,7 +124,7 @@ BEGIN
 		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
 		bb.PropertyName AS [BACKSHOP_PROP_NAME],
 		ii.LoanProgramDesc AS [LOAN_PROGRAM],
-		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], jj. RELATIONSHIP_ID AS [Relationship ID],
 		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
 		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
 		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
@@ -138,7 +141,7 @@ BEGIN
 		INNER JOIN (
 			tblControlMaster aa 
 			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
-			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId
+			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F'
 			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
 			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
 			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
@@ -160,7 +163,173 @@ BEGIN
 			AND a.Inactive = 0
 			--Gana: On 3/7/19, added condition to include ONLY ACTIVE reporting requirements - END
 		ORDER BY a.AsOfDate, ee.ServicerLoanNumber
-END				
+END	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--IF @REP_REQ_STATUS =  'OPEN'
+--BEGIN
+--	SELECT 
+--		a.ReportingRequirementId AS [MD_REP_REQ_ID],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
+--		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
+--		aa.ControlId AS [BACKSHOP_LOAN_IDENTIFIER],
+--		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
+--		bb.PropertyName AS [BACKSHOP_PROP_NAME],
+--		ii.LoanProgramDesc AS [LOAN_PROGRAM],
+--		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+--		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
+--		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
+--		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
+--		FORMAT(a.AsOfDate, 'MM/dd/yyyy') AS [AS_OF_DATE],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - BEGIN
+--		ISNULL(hh.ReportingRequirementStatusDesc, 'Open') AS [REQ_STATUS],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - END
+--		FORMAT(a.ReceivedDate, 'MM/dd/yyyy') AS [RXD_DATE], 
+--		FORMAT(DATEADD(dd, a.DaysDueWithin, a.AsOfDate), 'MM/dd/yyyy') AS [DUE_DATE], 
+--		FORMAT(a.AuditUpdateDate, 'MM/dd/yyyy') AS [UPDATE_DATE], 
+--		a.Comment AS [COMMENT]
+--	FROM 
+--		tblReportingRequirement a 
+--		INNER JOIN (
+--			tblControlMaster aa 
+--			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
+--			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F' AND dd.LienPositionCd_F = 'F'
+--			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
+--			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - END
+--			) ON aa.ControlId = a.ControlId_F
+--		INNER JOIN tblzCdReportingRequirementSubType c ON c.ReportingRequirementSubTypeCd = a.ReportingRequirementSubTypeCd_F AND c.ReportingRequirementTypeCd_F = a.ReportingRequirementTypeCd_F
+--		LEFT JOIN (tblSecUser ff 
+--			INNER JOIN tblContact gg ON gg.ContactID = ff.ContactID_F
+--			) ON ff.UserId = aa.AssetManager_UserID_F
+--		--Gana: On 3/6/19, added JOIN on table to display reporting status description instead of code - BEGIN
+--		LEFT JOIN tblzCdReportingRequirementStatus hh ON hh.ReportingRequirementStatusCd = a.ReportingRequirementStatusCd_F
+--		--Gana: On 3/6/19, added JOIN on table to display reporting status description instead of code - END
+--		LEFT JOIN tblzCdLoanProgram ii ON ii.LoanProgramCD = aa.LoanProgramCD_F
+--		WHERE 
+--			a.Inactive = 0 AND
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - BEGIN
+--			UPPER(jj.ASSET_MANAGER) IN (@ASSET_MANAGER) AND
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - END
+--			a.ReportingRequirementStatusCd_F IS NULL 
+--		ORDER BY a.AsOfDate, ee.ServicerLoanNumber
+--END
+--ELSE IF @REP_REQ_STATUS =  'ALL'
+--BEGIN
+--	SELECT 
+--		a.ReportingRequirementId AS [MD_REP_REQ_ID],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
+--		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
+--		aa.ControlId AS [BACKSHOP_LOAN_IDENTIFIER],
+--		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
+--		bb.PropertyName AS [BACKSHOP_PROP_NAME],
+--		ii.LoanProgramDesc AS [LOAN_PROGRAM],
+--		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+--		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
+--		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
+--		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
+--		FORMAT(a.AsOfDate, 'MM/dd/yyyy') AS [AS_OF_DATE],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - BEGIN
+--		ISNULL(hh.ReportingRequirementStatusDesc, 'Open') AS [REQ_STATUS],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - END
+--		FORMAT(a.ReceivedDate, 'MM/dd/yyyy') AS [RXD_DATE], 
+--		FORMAT(DATEADD(dd, a.DaysDueWithin, a.AsOfDate), 'MM/dd/yyyy') AS [DUE_DATE], 
+--		FORMAT(a.AuditUpdateDate, 'MM/dd/yyyy') AS [UPDATE_DATE], 
+--		a.Comment AS [COMMENT]
+--	FROM 
+--		tblReportingRequirement a 
+--		INNER JOIN (
+--			tblControlMaster aa 
+--			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
+--			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F'
+--			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
+--			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - END
+--			) ON aa.ControlId = a.ControlId_F
+--		INNER JOIN tblzCdReportingRequirementSubType c ON c.ReportingRequirementSubTypeCd = a.ReportingRequirementSubTypeCd_F AND c.ReportingRequirementTypeCd_F = a.ReportingRequirementTypeCd_F
+--		LEFT JOIN (tblSecUser ff 
+--			INNER JOIN tblContact gg ON gg.ContactID = ff.ContactID_F
+--			) ON ff.UserId = aa.AssetManager_UserID_F
+--		LEFT JOIN tblzCdReportingRequirementStatus hh ON hh.ReportingRequirementStatusCd = a.ReportingRequirementStatusCd_F
+--		LEFT JOIN tblzCdLoanProgram ii ON ii.LoanProgramCD = aa.LoanProgramCD_F
+--		WHERE 
+--			a.Inactive = 0 AND
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - BEGIN
+--			UPPER(jj.ASSET_MANAGER) IN (@ASSET_MANAGER) 
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - END
+--		ORDER BY a.AsOfDate, ee.ServicerLoanNumber
+--END
+--ELSE
+--BEGIN
+--	SELECT 
+--		a.ReportingRequirementId AS [MD_REP_REQ_ID],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - BEGIN
+--		UPPER(jj.ASSET_MANAGER) AS [ASSET_MANAGER],
+--		--Gana: On 3/6/19, changed to retrieve Asset Manager name from STR/CADM instead of Backshop - END
+--		aa.ControlId AS [BACKSHOP_LOAN_IDENTIFIER],
+--		ee.ServicerLoanNumber AS [STR_LOAN_IDENTIFIER],
+--		bb.PropertyName AS [BACKSHOP_PROP_NAME],
+--		ii.LoanProgramDesc AS [LOAN_PROGRAM],
+--		a.AssetManagementCovenantCategoryCd_F AS [REPORTING_FOR], 
+--		a.ReportingRequirementTypeCd_F AS [REPORTING_TYPE],
+--		c.ReportingRequirementSubTypeDesc AS [REPORTING_SUB_TYPE],
+--		a.DaysDueWithin AS [DAYS_DUE_WITHIN],
+--		FORMAT(a.AsOfDate, 'MM/dd/yyyy') AS [AS_OF_DATE],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - BEGIN
+--		ISNULL(hh.ReportingRequirementStatusDesc, 'Open') AS [REQ_STATUS],
+--		--Gana: On 3/6/19, changed to display reporting status description instead of code - END
+--		FORMAT(a.ReceivedDate, 'MM/dd/yyyy') AS [RXD_DATE], 
+--		FORMAT(DATEADD(dd, a.DaysDueWithin, a.AsOfDate), 'MM/dd/yyyy') AS [DUE_DATE], 
+--		FORMAT(a.AuditUpdateDate, 'MM/dd/yyyy') AS [UPDATE_DATE], 
+--		a.Comment AS [COMMENT]
+--	FROM 
+--		tblReportingRequirement a 
+--		INNER JOIN (
+--			tblControlMaster aa 
+--			INNER JOIN tblProperty bb ON aa.ControlId = bb.ControlId_F -- AND bb.PropertyTypeMajorCd_F = 'FMM'
+--			INNER JOIN tblNote dd ON dd.ControlId_F = aa.ControlId AND dd.LienPositionCd_F = 'F'
+--			INNER JOIN tblNoteExp ee ON ee.NoteId_F = dd.NoteId -- AND ee.ServicerLoanNumber IS NOT NULL
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - BEGIN
+--			INNER JOIN HSB_HIST.STRATEGY_EXTRACT jj ON jj.LOANNBR = ee.ServicerLoanNumber AND DATA_EFFDATE = (SELECT MAX(DATA_EFFDATE) FROM HSB_HIST.STRATEGY_EXTRACT)
+--			--Gana: On 3/6/19, added JOIN to validate if loan is still actively managed - END
+--			) ON aa.ControlId = a.ControlId_F
+--		INNER JOIN tblzCdReportingRequirementSubType c ON c.ReportingRequirementSubTypeCd = a.ReportingRequirementSubTypeCd_F AND c.ReportingRequirementTypeCd_F = a.ReportingRequirementTypeCd_F
+--		LEFT JOIN (tblSecUser ff 
+--			INNER JOIN tblContact gg ON gg.ContactID = ff.ContactID_F
+--			) ON ff.UserId = aa.AssetManager_UserID_F
+--		INNER JOIN tblzCdReportingRequirementStatus hh ON hh.ReportingRequirementStatusCd = a.ReportingRequirementStatusCd_F
+--		LEFT JOIN tblzCdLoanProgram ii ON ii.LoanProgramCD = aa.LoanProgramCD_F
+--		WHERE 
+--			a.Inactive = 0 AND
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - BEGIN
+--			UPPER(jj.ASSET_MANAGER) IN (@ASSET_MANAGER) AND
+--			--Gana: On 3/6/19, changed condition to lookup AM in CADM instead of Backshop - END
+--			a.ReportingRequirementStatusCd_F IN (@REP_REQ_STATUS) 
+--		ORDER BY a.AsOfDate, ee.ServicerLoanNumber
+--END				
 					
 
 		
